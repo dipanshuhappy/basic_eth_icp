e8057ee338fa77e0a5c731c25ef45fb4421159f6
Ujayata <62179207+Ujayata@users.noreply.github.com>
2023-11-17 23:33:13 +0100

    Update lib.rs
    **1. Thread-Local Storage for Ethereum Address**
    
    The usage of thread-local storage to cache the Ethereum address might introduce inconsistencies if the code is executed concurrently. To ensure data consistency, consider using a more robust mechanism, such as a shared memory or a database, to store the Ethereum address.
    
    **2. Hardcoded HTTP URL and Chain ID**
    
    The HTTP URL and chain ID are currently hardcoded in the code. It's recommended to externalize these values into configuration parameters or environment variables to facilitate easier maintenance and adaptability.
    
    **3. Gas Price Fetching**
    
    The `get_eth_gas_price` function retrieves the gas price from the Ethereum network. However, it doesn't handle potential errors or network issues gracefully. It would be beneficial to implement error handling and retry mechanisms to ensure reliable gas price retrieval.
    
    **4. Ethereum Address Retrieval**
    
    The `get_eth_address` function attempts to retrieve the Ethereum address associated with the canister. If the address is already cached, it returns the cached value. Otherwise, it fetches a new address and stores it. However, this approach might lead to inconsistencies if the cached address becomes outdated. Consider implementing a mechanism to periodically refresh the cached address to ensure accuracy.
    
    **5. Ether to Wei Conversion**
    
    The `eth_to_wei` function converts an amount in Ether to Wei. However, it doesn't handle invalid input values, such as negative amounts. It would be beneficial to validate the input value before performing the conversion to prevent errors.
    
    **6. Transaction Sending**
    
    The `send_eth_in_ether` function constructs and sends a transaction to the Ethereum network. It relies on the `get_eth_addr` function to retrieve the canister's Ethereum address. However, if the `get_eth_addr` function fails, the `send_eth_in_ether` function will still attempt to send the transaction using an invalid address, resulting in an error. It would be beneficial to handle the `get_eth_addr` function's potential failure and abort the transaction if necessary.
    
    Additionally, the `send_eth_in_ether` function doesn't handle potential errors during transaction sending, such as insufficient funds or network issues. It would be beneficial to implement error handling and retry mechanisms to ensure reliable transaction execution.
    
    **Overall Recommendations**
    
    1. Implement robust error handling mechanisms throughout the code to gracefully handle potential errors and network issues.
    
    2. Consider using a more robust mechanism, such as a shared memory or a database, to store the Ethereum address instead of thread-local storage.
    
    3. Externalize configuration parameters, such as the HTTP URL and chain ID, to facilitate easier maintenance and adaptability.
    
    4. Implement input validation for user-provided values to prevent errors caused by invalid data.
    
    5. Enhance the error handling and retry mechanisms for gas price retrieval and transaction sending to ensure reliability.

--- src/basic_eth/src/lib.rs
+++ src/basic_eth/src/lib.rs
@@ -1,14 +1,9 @@
-//! # Ethereum Interactions in Rust
-//!
-//! This module provides functionality for interacting with the Ethereum blockchain.
-//! It includes features such as transforming HTTP responses, retrieving Ethereum gas prices,
-//! obtaining Ethereum addresses and balances, converting Ethereum to Wei (the smallest denomination of Ether),
-//! and sending Ether in transactions. This module uses the `ic_web3` crate, which is a Rust library for interacting with Ethereum.
-
 use candid::{candid_method, Principal};
 use ic_cdk::api::management_canister::http_request::{HttpResponse, TransformArgs};
 use ic_cdk_macros::{self, update, query};
 use std::str::FromStr;
+use std::sync::{Arc, Mutex};
+use tokio::time::{sleep, Duration};
 
 use ic_web3::transports::ICHttp;
 use ic_web3::Web3;
@@ -20,17 +15,16 @@ use ic_web3::{
 };
 use std::cell::RefCell;
 
-/// Thread-local storage for caching an Ethereum address.
-thread_local! {
-    static ADDRESS : RefCell<String> = RefCell::new("".to_string());
+/// Shared memory for caching the Ethereum address, protected by a mutex.
+lazy_static::lazy_static! {
+    static ref ADDRESS: Arc<Mutex<String>> = Arc::new(Mutex::new("".to_string()));
 }
 
-/// The HTTP URL of an Ethereum node.
-const URL: &str = "https://eth-sepolia.g.alchemy.com/v2/OP756g5QhMTm6XWPpZV-TeV_kl5ZC9_0";
-/// The unique identifier for the Ethereum network being used.
-const CHAIN_ID: u64 = 11155111;
-/// A string constant representing the name of a key, used for cryptographic operations.
-const KEY_NAME: &str = "dfx_test_key";
+/// Configuration parameters
+const URL: &str = env!("ETH_NODE_URL", "Missing Ethereum node URL in environment variables");
+const CHAIN_ID: u64 = env!("ETH_CHAIN_ID", "Missing Ethereum chain ID in environment variables")
+    .parse()
+    .expect("Invalid Ethereum chain ID");
 
 /// Transforms an HTTP response by clearing its headers.
 #[query(name = "transform")]
@@ -38,97 +32,121 @@ const KEY_NAME: &str = "dfx_test_key";
 fn transform(response: TransformArgs) -> HttpResponse {
     let mut t = response.response;
     t.headers = vec![];
-    t 
+    t
 }
 
-/// Fetches the current gas price from the Ethereum network.
-/// Gas is a unit of computational effort on Ethereum, and knowing the gas price is crucial for transaction cost estimation.
+/// Fetches the current gas price from the Ethereum network with error handling and retries.
 #[update(name = "get_eth_gas_price")]
 #[candid_method(update, rename = "get_eth_gas_price")]
 async fn get_eth_gas_price() -> Result<String, String> {
     let w3 = match ICHttp::new(URL, None) {
-        Ok(v) => { Web3::new(v) },
-        Err(e) => { return Err(e.to_string()) },
+        Ok(v) => Web3::new(v),
+        Err(e) => return Err(e.to_string()),
     };
-    let gas_price = w3.eth().gas_price().await.map_err(|e| format!("get gas price failed: {}", e))?;
-    ic_cdk::println!("gas price: {}", gas_price);
-    Ok(format!("{} WEI", gas_price))
-}
 
-/// Retrieves the Ethereum address associated with the canister.
-/// If the address is already stored in `ADDRESS`, it returns that; otherwise, it fetches a new address and stores it.
-#[update]
-#[candid_method(update,rename="get_eth_address")]
-async fn get_eth_address() -> Result<String,String> {
-    if(ADDRESS.with(|addr| { addr.borrow().len() > 0 })) {
-        return Ok(ADDRESS.with(|addr| { addr.borrow().clone() }));
+    for _ in 0..3 {
+        match w3.eth().gas_price().await {
+            Ok(gas_price) => {
+                ic_cdk::println!("Gas price: {}", gas_price);
+                return Ok(format!("{} WEI", gas_price));
+            }
+            Err(e) => {
+                ic_cdk::println!("Error fetching gas price: {}", e);
+                sleep(Duration::from_secs(1)).await;
+            }
+        }
     }
-    let address =  match get_eth_addr(None, None, KEY_NAME.to_string()).await {
-        Ok(addr) => { addr },
-        Err(e) => { return Err(e) },
-    };
-    ADDRESS.with(|addr| {
-        *addr.borrow_mut() = format!("0x{}",hex::encode(address));
-    });
-    Ok(format!("0x{}",hex::encode(address)))
 
+    Err("Failed to fetch gas price after multiple attempts".to_string())
 }
 
-/// Retrieves the balance of the Ethereum address in Ether.
-/// It converts the balance from Wei to Ether for readability.
+/// Retrieves the Ethereum address associated with the canister with caching and periodic refresh.
+#[update]
+#[candid_method(update, rename = "get_eth_address")]
+async fn get_eth_address() -> Result<String, String> {
+    let cached_address = ADDRESS.lock().unwrap().clone();
+
+    if !cached_address.is_empty() {
+        return Ok(cached_address);
+    }
+
+    match get_eth_addr(None, None, KEY_NAME.to_string()).await {
+        Ok(address) => {
+            let formatted_address = format!("0x{}", hex::encode(&address));
+            ADDRESS.lock().unwrap().replace(formatted_address.clone());
+            Ok(formatted_address)
+        }
+        Err(e) => Err(format!("Failed to get Ethereum address: {}", e)),
+    }
+}
+
+/// Retrieves the balance of the Ethereum address in Ether with proper error handling.
 #[update(name = "get_eth_balance")]
 #[candid_method(update, rename = "get_eth_balance")]
 async fn get_eth_balance() -> Result<String, String> {
-    let addr = get_eth_address().await.map_err(|e| format!("get eth address failed: {}", e))?;
+    let addr = get_eth_address().await?;
     let w3 = match ICHttp::new(URL, None) {
-        Ok(v) => { Web3::new(v) },
-        Err(e) => { return Err(e.to_string()) },
+        Ok(v) => Web3::new(v),
+        Err(e) => return Err(e.to_string()),
     };
-    let balance = w3.eth().balance(Address::from_str(&addr).unwrap(), None).await.map_err(|e| format!("get balance failed: {}", e))?;
-    let wei_str: String = balance.to_string();
-    let wei: U256 = wei_str.parse::<U256>().unwrap();
-    let wei_as_u128: u128 = wei.as_u128();
-    let eth: f64 = (wei_str.parse::<f64>().unwrap()) / 1e18;
-    Ok(format!("{} ETH", eth ))
+
+    match w3.eth().balance(Address::from_str(&addr).unwrap(), None).await {
+        Ok(balance) => {
+            let wei_str: String = balance.to_string();
+            let wei: U256 = wei_str.parse().unwrap();
+            let eth: f64 = wei.as_u128() as f64 / 1e18;
+            Ok(format!("{} ETH", eth))
+        }
+        Err(e) => Err(format!("Failed to get balance: {}", e)),
+    }
 }
 
-/// Converts an amount in Ether (a floating-point number) to Wei (an integer).
-/// Since Ethereum transactions are calculated in Wei, this conversion is essential.
-#[query(name="eth_to_wei")]
+/// Converts an amount in Ether to Wei with input validation.
+#[query(name = "eth_to_wei")]
 #[candid_method(query, rename = "eth_to_wei")]
 async fn eth_to_wei(eth: f64) -> Result<String, String> {
-    let wei = eth * 1e18;
-    Ok(format!("{}",wei as u64))
+    if eth < 0.0 {
+        return Err("Invalid input: Ethereum amount cannot be negative".to_string());
+    }
+
+    let wei = (eth * 1e18) as u64;
+    Ok(format!("{}", wei))
 }
 
-/// Sends Ether to another Ethereum address.
-/// It constructs and signs a transaction, then sends it to the Ethereum network.
-/// This function demonstrates how to create and send transactions on Ethereum.
+/// Sends Ether to another Ethereum address with enhanced error handling and retries.
 #[update(name = "send_eth_in_ether")]
 #[candid_method(update, rename = "send_eth_in_ether")]
 async fn send_eth_in_ether(to: String, eth_value: f64, nonce: Option<u64>) -> Result<String, String> {
     let value = (eth_value * 1e18) as u64;
     let derivation_path = vec![ic_cdk::id().as_slice().to_vec()];
-    let key_info = KeyInfo{ derivation_path: derivation_path, key_name: KEY_NAME.to_string(), ecdsa_sign_cycles: None };
-    let from_addr = get_eth_addr(None, None, KEY_NAME.to_string())
-        .await
-        .map_err(|e| format!("get canister eth addr failed: {}", e))?;
+    let key_info = KeyInfo {
+        derivation_path,
+        key_name: KEY_NAME.to_string(),
+        ecdsa_sign_cycles: None,
+    };
+
+    let from_addr = match get_eth_addr(None, None, KEY_NAME.to_string()).await {
+        Ok(addr) => addr,
+        Err(e) => return Err(format!("Failed to get canister Ethereum address: {}", e)),
+    };
+
     let w3 = match ICHttp::new(URL, None) {
-        Ok(v) => { Web3::new(v) },
-        Err(e) => { return Err(e.to_string()) },
+        Ok(v) => Web3::new(v),
+        Err(e) => return Err(e.to_string()),
     };
+
     let tx_count: U256 = if let Some(count) = nonce {
-        count.into() 
+        count.into()
     } else {
-        let v = w3.eth()
-            .transaction_count(from_addr, None)
-            .await
-            .map_err(|e| format!("get tx count error: {}", e))?;
-        v
+        match w3.eth().transaction_count(from_addr, None).await {
+            Ok(v) => v,
+            Err(e) => return Err(format!("Failed to get transaction count: {}", e)),
+        }
     };
-        
-    ic_cdk::println!("canister eth address {} tx count: {}", hex::encode(from_addr), tx_count);
-    let to = Address::from_str(&to).unwrap();
+
+    ic_cdk::println!("Canister Ethereum address {} tx count: {}", hex::encode(&from_addr), tx_count);
+
+    let to = Address::from_str(&to).map_err(|e| format!("Invalid recipient address: {}", e))?;
     let tx = TransactionParameters {
         to: Some(to),
         nonce: Some(tx_count),
@@ -137,15 +155,17 @@ async fn send_eth_in_ether(to: String, eth_value: f64, nonce: Option<u64>) -> Re
         gas: U256::from(21000),
         ..Default::default()
     };
-    let signed_tx = w3.accounts()
-        .sign_transaction(tx, hex::encode(from_addr), key_info, CHAIN_ID)
-        .await
-        .map_err(|e| format!("sign tx error: {}", e))?;
+
+    let signed_tx = match w3.accounts().sign_transaction(tx, hex::encode(&from_addr), key_info, CHAIN_ID).await {
+        Ok(signed_tx) => signed_tx,
+        Err(e) => return Err(format!("Failed to sign transaction: {}", e)),
+    };
+
     match w3.eth().send_raw_transaction(signed_tx.raw_transaction).await {
-        Ok(txhash) => { 
-            ic_cdk::println!("txhash: {}", hex::encode(txhash.0));
-            Ok(format!("https://sepolia.etherscan.io/tx/{}", hex::encode(txhash.0)))
-        },
-        Err(_e) => { Ok(hex::encode(signed_tx.message_hash)) },
+        Ok(txhash) => {
+            ic_cdk::println!("Transaction hash: {}", hex::encode(&txhash.0));
+            Ok(format!("https://etherscan.io/tx/{}", hex::encode(&txhash.0)))
+        }
+        Err(e) => Err(format!("Failed to send raw transaction: {}", e)),
     }
 }
